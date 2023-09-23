83bcfdae7977f77bef407a8caed7b285bcd88f64
Guilherme Garcia Ortega de Oliveira <111614180+GarciaGGOO@users.noreply.github.com>
2023-09-22 21:34:46 -0300

    Add files via upload

---
+++ src/calculadora/OperacoesMatematicas.java
@@ -0,0 +1,17 @@
+package calculadora;
+
+public class OperacoesMatematicas {
+	
+	public double somar(double a, double b) {
+		return a + b;
+	}
+	public double subtrair(double a, double b) {
+		return a - b;
+	}
+	public static double multiplicar(double a, double b) {
+        return a * b;
+    }
+	public static double dividir(double a, double b) {
+	    return a / b;
+	}
+}
---
+++ src/calculadora/SuppressWarnigs.java
@@ -0,0 +1,7 @@
+package calculadora;
+
+public @interface SuppressWarnigs {
+
+	String value();
+
+}
